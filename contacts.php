aede7a3f588eeb3c7b44014a5791a145dbcc1907
Shokhrukh <58341874+nextin94@users.noreply.github.com>
2020-03-14 14:42:34 +0300

    contacts.php

---
+++ contacts.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <title>contacts</title>
+    </head>
+    <body>
+        <div id="wrapper">
+            <header>
+                header
+            </header>
+            <main>
+                contacts
+            </main>
+            <footer>
+                footer
+            </footer>
+        </div>
+    </body>
+</html>
