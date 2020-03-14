af22536dad16102bfd74dd55050f97671a770aac
Shokhrukh <58341874+nextin94@users.noreply.github.com>
2020-03-14 14:42:07 +0300

    about.php

---
+++ about.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <title>about</title>
+    </head>
+    <body>
+        <div id="wrapper">
+            <header>
+                header
+            </header>
+            <main>
+                aboute
+            </main>
+            <footer>
+                footer
+            </footer>
+        </div>
+    </body>
+</html>
